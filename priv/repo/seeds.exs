a16b5af97187130c0f9fd785e1966671e5a45ccd
andre.gusmao <albg-lnx@albg-win>
2022-10-15 23:59:58 -0300

    criação de um usuário padrão em seeds.ex

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias HackDay13.Persons
+
+Persons.create_person(%{
+  "firstname" => "André",
+  "surname" => "Gusmão",
+  "email" => "andre.gusmao@rcky.com.br"
+})
